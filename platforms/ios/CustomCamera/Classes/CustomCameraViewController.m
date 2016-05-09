30865732cc1b402fbf0a734701bd8474c5c3ab74
mcookman <mcookman@expansionhealth.com>
2016-05-09 16:16:36 -0400

    Add files via upload

--- platforms/ios/CustomCamera/Classes/CustomCameraViewController.m
+++ platforms/ios/CustomCamera/Classes/CustomCameraViewController.m
@@ -120,7 +120,7 @@ static const CGFloat kAspectRatio = 125.0f / 86;
 }
 
 - (void)setTopText:(NSString *)s {
-  // _topTextString = [[NSString alloc] initWithFormat:@"%@", s];
+   _topTextString = [[NSString alloc] initWithFormat:@"%@", s];
 }
 
 - (void)viewWillLayoutSubviews {
@@ -235,8 +235,8 @@ static const CGFloat kAspectRatio = 125.0f / 86;
                                     kCaptureButtonHeightTablet + (kCaptureButtonVerticalInsetTablet * 2));
     
 	UIFont* font = [UIFont fontWithName:@"Arial" size:26];
-	[_topTextLabel setFrame: CGRectMake(kHorizontalInsetTablet, kVerticalInsetTablet, bounds.size.width - kBorderImageWidthTablet, kBorderImageHeightTablet)];
-	[_topTextLabel setText: @"Default Text"];
+	[_topTextLabel setFrame: CGRectMake(kHorizontalInsetTablet, 10, bounds.size.width - (2* kBorderImageWidthTablet), kBorderImageHeightTablet)];
+	[_topTextLabel setText: _topTextString];
 	[_topTextLabel setFont: font];
 	[_topTextLabel setBackgroundColor: [UIColor clearColor]];
 	UIColor *color = [UIColor whiteColor];
