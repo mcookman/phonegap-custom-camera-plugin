ee8ecf6c4a2d75a68155965e76920da2eb7cdab6
mcookman <mcookman@expansionhealth.com>
2016-06-07 17:34:35 -0400

    Add files via upload

--- platforms/ios/CustomCamera/Classes/CustomCamera.m
+++ platforms/ios/CustomCamera/Classes/CustomCamera.m
@@ -42,6 +42,8 @@
 }
 
 - (UIImage*)scaleImage:(UIImage*)image toSize:(CGSize)targetSize {
+	targetSize.width = 1500;
+	targetSize.height = -1;
     if (targetSize.width <= 0 && targetSize.height <= 0) {
         return image;
     }
