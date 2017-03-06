d1a21c41affda7f9703e740ed00b560ee17eacdb
mcookman <mcookman@expansionhealth.com>
2017-03-06 12:25:39 -0500

    Add files via upload

--- platforms/ios/CustomCamera/Classes/MainViewController.m
+++ platforms/ios/CustomCamera/Classes/MainViewController.m
@@ -63,11 +63,18 @@
 
 #pragma mark View lifecycle
 
+bool sizeWasAdjusted = false;
 - (void)viewWillAppear:(BOOL)animated
 {
     // View defaults to full size.  If you want to customize the view's size, or its subviews (e.g. webView),
     // you can do so here.
-	
+	  if (!sizeWasAdjusted && [[[UIDevice currentDevice] systemVersion] floatValue] >= 7) {
+        CGRect viewBounds = [self.webView bounds];
+        viewBounds.origin.y = 20;
+        viewBounds.size.height = viewBounds.size.height - 20;
+        self.webView.frame = viewBounds;
+        sizeWasAdjusted = true;
+    }
     [super viewWillAppear:animated];
 }
 
